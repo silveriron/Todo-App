a409ea8807f4b4d248e66d22006bfcba204c1e22
kichul0227 <kichul7493@gmail.com>
2023-04-28 08:54:58 +0900

    fix: 메인페이지 refresh 토큰 확인 후 페이지 리다이렉트 방식 변경

--- todo-frontend/src/pages/index.tsx
+++ todo-frontend/src/pages/index.tsx
@@ -1,21 +1,18 @@
 import { useEffect } from "react";
 import { getAccessToken } from "@/lib/api/api";
-import { useRouter } from "next/router";
 
 const Main = () => {
-  const router = useRouter();
-
   useEffect(() => {
     getAccessToken()
       .then((res) => {
         if (res.status === 201) {
-          router.push("/todo");
+          window.location.href = "/todo";
         } else {
-          router.push("/auth/signin");
+          window.location.href = "/auth/signin";
         }
       })
       .catch((err) => {
-        router.push("/auth/signin");
+        window.location.href = "/auth/signin";
       });
   });
 

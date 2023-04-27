4c82676f32bd9a0d93053e323da9c4f93f3276cb
kichul0227 <kichul7493@gmail.com>
2023-04-28 08:21:01 +0900

    fix: 카카오 로그인 관련 주소 ec2로 변경

--- todo-frontend/src/components/auth/kakao/KaKaoLogin.tsx
+++ todo-frontend/src/components/auth/kakao/KaKaoLogin.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import React from "react";
 
-const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+const REDIRECT_URI =
+  "http://ec2-52-78-166-75.ap-northeast-2.compute.amazonaws.com/auth/kakao/callback";
 
 const href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
--- todo-frontend/src/components/common/Layout/Menu/SideMenu/SideMenu.tsx
+++ todo-frontend/src/components/common/Layout/Menu/SideMenu/SideMenu.tsx
@@ -1,23 +1,10 @@
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import Header from "./Header";
 import LogoutButton from "./LogoutButton";
 import { menuState } from "@/store/atoms/menuState";
-import Text from "@/components/common/typography/Text";
-import { useMutation } from "@tanstack/react-query";
-import { signOut } from "@/lib/api/api";
 
 const SideMenu = () => {
-  const [isMenu, SetIsMenu] = useRecoilState(menuState);
-  const { mutate } = useMutation(signOut, {
-    onSuccess: () => {
-      SetIsMenu(false);
-      window.location.href = "/auth/signin";
-    },
-  });
-
-  const signOutHandler = () => {
-    mutate();
-  };
+  const isMenu = useRecoilValue(menuState);
 
   const w = isMenu ? "w-[16%]" : "w-[0px]";
   const minW = isMenu ? "min-w-[160px]" : "min-w-[0px]";
@@ -30,11 +17,7 @@ const SideMenu = () => {
       <div className="w-full h-full justify-between flex flex-col p-3 border-r-2">
         <div className="flex flex-col gap-2">
           <Header />
-          <div className="flex justify-between items-center">
-            <button onClick={signOutHandler}>
-              <Text>Logout</Text>
-            </button>
-          </div>
+          <div className="flex justify-between items-center"></div>
         </div>
         <LogoutButton />
       </div>

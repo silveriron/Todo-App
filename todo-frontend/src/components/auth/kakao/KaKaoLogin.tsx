b205b0775f78ef3e44412eab881e9fa3a41bf97f
kichul <kichul7493@gmail.com>
2023-04-29 00:24:00 +0900

    fix: 카카오 로그인 버튼

--- todo-frontend/src/components/auth/kakao/KaKaoLogin.tsx
+++ todo-frontend/src/components/auth/kakao/KaKaoLogin.tsx
@@ -6,7 +6,14 @@ const REDIRECT_URI = "https://todo-app.shop/auth/kakao/callback";
 const href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const KaKaoLogin = () => {
-  return <Link href={href}>KAKAO Login</Link>;
+  return (
+    <Link
+      className="h-[35px] flex justify-center items-center rounded font-bold bg-[#FAE64D] mb-2"
+      href={href}
+    >
+      카카오로 1초만에 로그인
+    </Link>
+  );
 };
 
 export default KaKaoLogin;

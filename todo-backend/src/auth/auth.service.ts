25fbd50c9dbb84beb937322e59e9b410d8278dc5
kichul <kichul7493@gmail.com>
2023-04-28 23:14:01 +0900

    fix: 도메인 주소 적용

--- todo-backend/src/auth/auth.service.ts
+++ todo-backend/src/auth/auth.service.ts
@@ -114,8 +114,7 @@ export class AuthService {
           params: {
             grant_type: 'authorization_code',
             client_id: process.env.KAKAO_CLIENT_ID,
-            redirect_uri:
-              'http://ec2-52-78-166-75.ap-northeast-2.compute.amazonaws.com/auth/kakao/callback',
+            redirect_uri: 'https://todo-app.shop/auth/kakao/callback',
             code,
           },
         },

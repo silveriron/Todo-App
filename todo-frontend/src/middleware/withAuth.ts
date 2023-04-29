5c5de8931baaa2576e9e553314648038f22ad588
kichul <kichul7493@gmail.com>
2023-04-29 21:47:13 +0900

    fix: refresh 주소 http -> https 변경

--- todo-frontend/src/middleware/withAuth.ts
+++ todo-frontend/src/middleware/withAuth.ts
@@ -6,7 +6,7 @@ export async function withAuth(req: NextRequest) {
   try {
     const refresh_token = req.cookies.get("refresh_token")?.value;
 
-    const res = await fetch("http://localhost:3001/api/v1/auth/refresh", {
+    const res = await fetch("https://todo-app.shop:8443/api/v1/auth/refresh", {
       method: "POST",
       body: JSON.stringify({ refresh_token }),
       headers: {
